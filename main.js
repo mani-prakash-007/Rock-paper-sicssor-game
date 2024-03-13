da5a976561c9af5ff8250e635624605769b390f6
mani-prakash-007 <manip8072@gmail.com>
2024-03-13 10:24:01 +0530

    Add Alert for Comp choice

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
 let playgame = confirm("Shall we play Rock, Paper, Scissors");
 
 if (playgame) {
-    do {
-        RockPaperScissor();
-        var replay = confirm("Play Again..?");
-    } while (replay);
-    alert("Thanks for Playing");
+  do {
+    RockPaperScissor();
+    var replay = confirm("Play Again..?");
+  } while (replay);
+  alert("Thanks for Playing");
 } else {
-    alert("We will play next time...");
+  alert("We will play next time...");
 }
 
 function RockPaperScissor() {
-    let playerchoice = prompt("Enter Rock / Paper / Scissors.");
-    if (playerchoice === null || playerchoice.trim() === "") {
-        alert("Empty Input");
-        return;
-    }
+  let playerchoice = prompt("Enter Rock / Paper / Scissors.");
+  if (playerchoice === null || playerchoice.trim() === "") {
+    alert("Empty Input");
+    return;
+  }
 
-    playerchoice = playerchoice.trim().toLowerCase();
-    let computerOption = ["rock", "paper", "scissors"];
-    let number = Math.random() * 3;
-    let roundoff = Math.floor(number);
-    let computerchoice = computerOption[roundoff];
-    
-    let result =
-        playerchoice === computerchoice ? "Tie Game" :
-        (playerchoice === "rock" && computerchoice === "paper") ? "Computer Wins..!" :
-        (playerchoice === "rock" && computerchoice === "scissors") ? "You Won..!" :
-        (playerchoice === "scissors" && computerchoice === "paper") ? "You Won..!" :
-        (playerchoice === "scissors" && computerchoice === "rock") ? "Computer Wins..!" :
-        (playerchoice === "paper" && computerchoice === "rock") ? "You Won..!" :
-        (playerchoice === "paper" && computerchoice === "scissors") ? "Computer Wins..!" :
-        "Invalid Input. Please Enter a valid Input.";
+  playerchoice = playerchoice.trim().toLowerCase();
+  let computerOption = ["rock", "paper", "scissors"];
+  let number = Math.random() * 3;
+  let roundoff = Math.floor(number);
+  let computerchoice = computerOption[roundoff];
+  alert(`Computer says : ${computerchoice}`);
 
-    alert(result);
+  let result =
+    playerchoice === computerchoice
+      ? "Tie Game"
+      : playerchoice === "rock" && computerchoice === "paper"
+      ? "Computer Wins..!"
+      : playerchoice === "rock" && computerchoice === "scissors"
+      ? "You Won..!"
+      : playerchoice === "scissors" && computerchoice === "paper"
+      ? "You Won..!"
+      : playerchoice === "scissors" && computerchoice === "rock"
+      ? "Computer Wins..!"
+      : playerchoice === "paper" && computerchoice === "rock"
+      ? "You Won..!"
+      : playerchoice === "paper" && computerchoice === "scissors"
+      ? "Computer Wins..!"
+      : "Invalid Input. Please Enter a valid Input.";
+
+  alert(result);
 }
